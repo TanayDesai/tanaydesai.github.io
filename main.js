d2de2d5bea4141df990435aa7e4e635bbec63de6
Tanay Desai <78469094+TanayDesai@users.noreply.github.com>
2022-08-08 20:48:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function redirect(id){
+  
+    if (id=="projects"){
+      window.location = "projects.html";
+      
+    }
+    else if(id=="home"){
+      window.location = "index.html";
+      
+    }
+    else if(id=="art"){
+      window.location = "art.html";
+      
+    }
+
+    
+      
+  }
